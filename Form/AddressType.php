8854145cb1dc03681254fe2d08ba7d83350d06a5
Maniac25 <niato@free.fr>
2015-11-16 11:29:05 +0100

    suppression construct  addressType

--- Form/AddressType.php
+++ Form/AddressType.php
@@ -8,20 +8,17 @@ use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 class AddressType extends AbstractType {
 
-    private $countries;
-
-    public function __construct($em, $locale) {
-        $countryRepo = $em->getRepository('ZENLocaleBundle:Country');
-        $this->countries = $countryRepo->findAll($locale);
-    }
-
     /**
      * @param FormBuilderInterface $builder
      * @param array $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-                ->add('country', 'entity', ['label' => 'address.country', 'class' => 'ZENLocaleBundle:Country', 'choices' => $this->countries, 'attr' => ['field_class' => 'select-search']])
+                ->add('country', 'entity', [
+                    'label' => 'address.country',
+                    'class' => 'ZENLocaleBundle:Country',
+                    'attr' => ['field_class' => 'select-search']
+                ])
                 ->add('zip', null, ['label' => 'address.zip'])
                 ->add('city', null, ['label' => 'address.city'])
                 ->add('street', null, ['label' => 'address.street'])
